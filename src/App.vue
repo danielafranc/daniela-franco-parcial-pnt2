80b486eed877a6cb7c160e7a1f27d1077e71b333
Daniela Franco <daniela.franco@allata.com>
2025-10-27 20:00:30 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,84 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app container mt-4">
+    <h1>Convertidor de Temperatura</h1>
+    <h2>Alumna: Daniela Franco</h2>
+    <a href="https://github.com/danielafranc/daniela-franco-parcial-pnt2">Repositorio en GitHub</a>
+    <hr>
+    <div>
+      <form @submit.prevent class="mb-4">
+        <label for="celsius">Ingresar Temperatura</label>
+        <input 
+          type="number" 
+          id="celsius"
+          v-model.number="temperaturaCelsius"
+          name="celsius"
+          class="form-control d-inline-block w-25 ms-2"
+          required>
+      </form>
+
+      <div class="resultados">
+        <table class="table table-bordered">
+          <thead class="table-light">
+            <tr>
+              <th>Escala</th>
+              <th>Temperatura</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Fahrenheit</td>
+              <td :style="{ color: colorTemperatura }">{{temperaturaFahrenheit}}°F</td>
+            </tr>
+            <tr>
+              <td>Kelvin</td>
+              <td :style="{ color: colorTemperatura }">{{temperaturaKelvin}}°K</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <Respuestas />
+  </div>
 </template>
+
+<script>
+import { ref, computed } from 'vue';
+import Respuestas from './components/Respuestas.vue';
+
+export default {
+  components: {
+    Respuestas
+  },
+  setup() {
+    const temperaturaCelsius = ref(0);
+
+    const temperaturaFahrenheit = computed(() => {
+      return (temperaturaCelsius.value * 9/5) + 32;
+    });
+
+    const temperaturaKelvin = computed(() => {
+      return temperaturaCelsius.value + 273.15;
+    });
+
+    const colorTemperatura = computed(() => {
+      if (temperaturaCelsius.value <= 0) {
+        return 'blue';
+      } else if (temperaturaCelsius.value > 0 && temperaturaCelsius.value < 15) {
+        return 'magenta';
+      } else {
+        return 'red';
+      }
+    });
+
+    return {
+      temperaturaCelsius,
+      temperaturaFahrenheit,
+      temperaturaKelvin,
+      colorTemperatura
+    };
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Respuestas.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Pregunta 1: Como es la forma reducida de v-on:click?</p>
+    <p> <span :style="{'font-weight': 'bold'}">Respuesta:</span> C</p>
+
+    <p>Pregunta 2: El modificador trim recorta...</p>
+    <p> <span :style="{'font-weight': 'bold'}">Respuesta:</span> C</p>
+
+    <p>Pregunta 3: La propiedad routes en la definición del router de Vue.js tiene como función:</p>
+    <p> <span :style="{'font-weight': 'bold'}">Respuesta:</span> A, B</p>
+
+</template>
